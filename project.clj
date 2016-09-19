c340fc87a3c9a5ad538afa8fe8a8efc428e74bed
Brian Noguchi <brian.noguchi@gmail.com>
2016-09-19 13:03:26 -0700

    :monkeypatch-clojure-test false workaround

--- project.clj
+++ project.clj
@@ -9,4 +9,8 @@
                  [org.clojure/test.check "0.9.0"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5390"]
                                   [org.clojure/tools.namespace "0.3.0-alpha3"]]}}
+
+  ; https://github.com/technomancy/leiningen/issues/2173
+  :monkeypatch-clojure-test false
+
   :plugins [[lein-cloverage "1.0.7-SNAPSHOT"]])
