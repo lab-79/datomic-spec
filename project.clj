0b3e1702a5f7cede4398d0b553999612551a278f
Brian Noguchi <brian.noguchi@gmail.com>
2016-09-19 12:08:42 -0700

    Mv src/clj to src/cljc

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/lab-79/datomic-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clj"]
+  :source-paths ["src/cljc"]
   :test-paths ["test"]
   :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
                  [org.clojure/test.check "0.9.0"]]
