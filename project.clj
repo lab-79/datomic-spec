25d5d2cf09ab668f24f3612f0ef76c12b7bdf3eb
Brian Noguchi <brian.noguchi@gmail.com>
2017-03-15 19:35:35 -0700

    Bump clojure to alpha15. Bump clojure-spec-helpers to alpha8

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/cljc"]
   :test-paths ["test"]
-  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [org.clojure/clojurescript "1.9.494"]
                  [org.clojure/test.check "0.9.0"]
-                 [lab79/clojure-spec-helpers "0.1.0-alpha7"]]
+                 [lab79/clojure-spec-helpers "0.1.0-alpha8"]]
   :profiles {:dev {:dependencies [[com.datomic/datomic-free "0.9.5561"]
                                   [org.clojure/tools.namespace "0.3.0-alpha3"]]}}
 
