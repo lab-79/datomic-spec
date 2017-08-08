7c191f873d7daa83e55e5989f4cd0996fae33d75
Brian Noguchi <brian.noguchi@gmail.com>
2017-08-07 17:29:26 -0700

    Version 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lab79/datomic-spec "0.2.0-alpha12-SNAPSHOT"
+(defproject lab79/datomic-spec "1.0.0"
   :description "clojure.spec for Datomic"
   :url "https://github.com/lab-79/datomic-spec"
   :license {:name "Eclipse Public License"
